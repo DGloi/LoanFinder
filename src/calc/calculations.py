cd124bd28d4fe6515829b8f030e1ca33669cedc7
Dorian <dorian.gloinec@protonmail.com>
2022-10-30 18:24:15 +0100

    Update calculations.py

--- src/calc/calculations.py
+++ src/calc/calculations.py
@@ -1,13 +1,13 @@
 """
-calculation.py contains multiple financial ratio calculation needed 
+calculation.py calculates multiple financial ratio  needed 
 to determine if one can apply to a loan or not
 """
 
 def monthly_debt_ratio(monthly_debt_payment, monthly_income):
+    monthly_debt_ratio= int(monthly_debt_payment) / int(monthly_income)
     return monthly_debt_ratio
 
 
-
-
 def loan_to_value_ratio (loan_amount,home_value):
+    loan_to_value_ratio= int(loan_amount)/int(home_value)
     return loan_to_value_ratio
