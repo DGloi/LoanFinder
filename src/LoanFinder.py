ef319467d831ab4db6807730770435985ca67d6a
Dorian <dorian.gloinec@protonmail.com>
2022-10-31 17:16:59 +0100

    app update

--- src/LoanFinder.py
+++ src/LoanFinder.py
@@ -5,6 +5,7 @@ on which a user can apply, depending on his project and his financial situation.
 """
 
 #importing libraries
+from mmap import PAGESIZE
 import sys
 import csv
 import fire
@@ -22,9 +23,25 @@ from filter.debt_to_income import filter_debt_to_income
 from filter.loan_to_value import filter_loan_to_value
 from filter.max_loan_size import filter_max_loan_size
 
+if __name__=="__main__":
+    fire.Fire(run_app)
+
+def get_user_info():
+    debt = q.text("What's your current amount of monthly debt?").ask()
+    income = q.text("What's your total monthly income?").ask()
+    loan_amount = q.text("What's your desired loan amount?").ask()
+    home_value = q.text("What's your home value?").ask()
+
+
 def banks_data():
     csvpath=q.text("Please provide banks info file location (.csv) ")
     csvpath=Path(csvpath)
     if not csvpath.exists():
         sys.exit(f"File not found on this  directory: {csvpath}")
     return import_csv(csvpath)
+
+def run():
+    debt,income,loan_amount,home_value=get_user_info()
+    
+    sentence="Thanks for using LoanFinder, we hope you found what you need."
+    return sentence 
