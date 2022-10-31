cb8de6c3230ad71e1d4315f36a8ff4414136d77f
Dorian <dorian.gloinec@protonmail.com>
2022-10-31 17:28:54 +0100

    app update

--- src/LoanFinder.py
+++ src/LoanFinder.py
@@ -5,7 +5,6 @@ on which a user can apply, depending on his project and his financial situation.
 """
 
 #importing libraries
-from mmap import PAGESIZE
 import sys
 import csv
 import fire
@@ -23,14 +22,19 @@ from filter.debt_to_income import filter_debt_to_income
 from filter.loan_to_value import filter_loan_to_value
 from filter.max_loan_size import filter_max_loan_size
 
-if __name__=="__main__":
-    fire.Fire(run_app)
 
 def get_user_info():
     debt = q.text("What's your current amount of monthly debt?").ask()
     income = q.text("What's your total monthly income?").ask()
     loan_amount = q.text("What's your desired loan amount?").ask()
     home_value = q.text("What's your home value?").ask()
+
+    debt=float(debt)
+    income=float(income)
+    loan_amount=float(loan_amount)
+    home_value=float(home_value)
+
+    return debt,income,loan_amount,home_value
 
 
 def banks_data():
@@ -40,8 +44,20 @@ def banks_data():
         sys.exit(f"File not found on this  directory: {csvpath}")
     return import_csv(csvpath)
 
-def run():
+
+def elligible_loans:
+    return 
+
+
+
+
+
+def run_app():
     debt,income,loan_amount,home_value=get_user_info()
-    
+
     sentence="Thanks for using LoanFinder, we hope you found what you need."
     return sentence 
+
+
+if __name__=="__main__":
+    fire.Fire(run_app)
